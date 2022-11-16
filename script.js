9db3a44f4408efc53fc31116831e3bfa33c784a5
Ruben Villagrana <111696775+RubenVillagrana@users.noreply.github.com>
2022-11-15 22:58:46 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 // 1. Write a function min that takes two numbers and returns the smallest one of the pair.
 // Examples:
+
+console.log(Math.min(4,2,));
+// expected output: 2
+
+
 // console.log(min(0, 10)); // will output 0.
 // console.log(min(0, -10)); // will output -10
 
 // 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
 // Example Output:
+
+function calculateCircumference(radius) {
+    return 2 * Math.PI * radius;
+  }
+  
+  console.log(Math.PI);
+  // expected output: 3.14159265358979
+
+  console.log(calculateCircumference(5));
+// expected output: 31.41592653589793  
 // console.log(calculateCircumference(5)); // will output 31.4
+
 
 // 3. Write a function searchString that takes a character and then returns how many times it appears in another string argument.
 // Example Output:
+
 // console.log(searchString(“s”, “mississippi”)); // will output 4
 // console.log(searchString(“q”, “mississippi”)); // will output 0
